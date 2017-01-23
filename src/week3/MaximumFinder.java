e61e0baf67cfedfd1d8717f5a4d4062d896018d0
Wallace Balaniuc <wbalaniu@my.centennialcollege.ca>
2017-01-23 11:00:40 -0500

    Started week 3 class examples

---
+++ src/week3/MaximumFinder.java
@@ -0,0 +1,20 @@
+package week3;
+
+import java.util.Scanner;
+
+public class MaximumFinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		// Prompt the user to enter values
+		System.out.print("Enter three floating-point values separated by spaces: " );
+		
+		// Store values into variables.
+		double number1 = input.nextDouble();
+		double number2 = input.nextDouble();
+		double number3 = input.nextDouble();
+	}
+
+}
