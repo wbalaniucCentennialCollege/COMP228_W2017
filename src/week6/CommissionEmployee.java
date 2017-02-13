3e5302b730d070adc0f939a0c90a81f068de68e5
Wallace Balaniuc <wbalaniu@my.centennialcollege.ca>
2017-02-13 11:35:29 -0500

    Finished commission employee

--- src/week6/CommissionEmployee.java
+++ src/week6/CommissionEmployee.java
@@ -1,15 +1,15 @@
 package week6;
 
-public class CommissionEmployee {
-	private final String firstName;
-	private final String lastName;
-	private final String socialSecurityNumber;
+public class CommissionEmployee extends Employee {
 	private double grossSales;
 	private double commissionRate;
 	
 	// Constructor
-	public CommissionEmployee(String firstName, String lastName, String ssn, 
+	public CommissionEmployee(String firstName, String lastName, String socialSecurityNumber, 
 			double grossSales, double commissionRate) {
+		
+		super(firstName, lastName, socialSecurityNumber);
+		
 		// Validate grossSale number
 		if(grossSales < 0.0){
 			throw new IllegalArgumentException("Gross sales must be >= 0.0");
@@ -20,9 +20,6 @@ public class CommissionEmployee {
 			throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
 		}
 		
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.socialSecurityNumber = ssn;
 		this.grossSales = grossSales;
 		this.commissionRate = commissionRate;
 	}
@@ -52,29 +49,17 @@ public class CommissionEmployee {
 		
 		this.commissionRate = commissionRate;
 	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public String getSocialSecurityNumber() {
-		return socialSecurityNumber;
-	}
 	
 	// Calculates earnings
+	@Override
 	public double earnings() {
 		return getCommissionRate() * getGrossSales();
 	}
 	
 	@Override
 	public String toString() {
-		return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f", 
-				"base-salaried commission employee", getFirstName(), getLastName(),
-				"social security number", getSocialSecurityNumber(),
+		return String.format("%s %s%n%s $%.2f; %s: %.2f", 
+				"Commission employee", super.toString(),
 				"gross sales", getGrossSales(),
 				"commission rate", getCommissionRate());
 	}
--- src/week6/SalariedEmployee.java
+++ src/week6/SalariedEmployee.java
@@ -4,15 +4,55 @@ public class SalariedEmployee extends Employee {
 
 	private double weeklySalary;
 	
+	// Constructor
 	public SalariedEmployee(String firstName, String lastName, 
-			String socialSecuityNumber, double weeklySalary) {
+			String socialSecurityNumber, double weeklySalary) {
+		super(firstName, lastName, socialSecurityNumber);
 		
+		// Check if weekly salary is invalid
+		if(weeklySalary < 0.0) {
+			throw new IllegalArgumentException("Weekly salary must be >= 0.0");
+		}
+		
+		this.weeklySalary = weeklySalary;
 	}
 	
-	@Override
-	public double earnings() {
-		// TODO Auto-generated method stub
-		return 0;
+	// Getter and setters
+	public double getWeeklySalary() {
+		return weeklySalary;
 	}
 
+	public void setWeeklySalary(double weeklySalary) {
+		if(weeklySalary < 0.0) {
+			throw new IllegalArgumentException("Weekly salary must be >= 0.0");
+		}
+		
+		this.weeklySalary = weeklySalary;
+	}
+
+	@Override
+	public double earnings() {
+		return getWeeklySalary();
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Salaried Employee: %s%n%s $%.2f", super.toString(),
+				"weekly salary", getWeeklySalary());
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
