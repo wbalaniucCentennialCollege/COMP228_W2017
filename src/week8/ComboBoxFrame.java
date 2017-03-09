7060966933b9d592a4d160066c9362f9834fee36
Wallace Balaniuc <wbalaniu@my.centennialcollege.ca>
2017-03-09 09:50:54 -0500

    Completed ComboBoxFrame example

--- src/week8/ComboBoxFrame.java
+++ src/week8/ComboBoxFrame.java
@@ -1,5 +1,65 @@
 package week8;
 
-public class ComboBoxFrame {
+import java.awt.FlowLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ComboBoxFrame extends JFrame {
+	private final JComboBox<String> imagesJComboBox;
+	private final JLabel label;
+	
+	private static final String[] names = 
+		{"bug1.gif", "bug2.gif", "travelbug.gif", "buganim.gif"};
+	
+	private final Icon[] icons = {
+			new ImageIcon("images/" + names[0]),
+			new ImageIcon("images/" + names[1]),
+			new ImageIcon("images/" + names[2]),
+			new ImageIcon("images/" + names[3])};
+	
+	public ComboBoxFrame() {
+		super("Testing JComboBox");
+		setLayout(new FlowLayout());
+		
+		imagesJComboBox = new JComboBox<String>(names);
+		imagesJComboBox.setMaximumRowCount(3);
+		
+		imagesJComboBox.addItemListener(
+			new ItemListener()
+			{
+				@Override
+				public void itemStateChanged(ItemEvent e) {
+					// TODO Auto-generated method stub
+					if(e.getStateChange() == ItemEvent.SELECTED) {
+						label.setIcon(icons[imagesJComboBox.getSelectedIndex()]);
+					}
+				}
+			} // end to our annoymous class
+		); // End call to addItemListener
+		
+		add(imagesJComboBox);
+		label = new JLabel(icons[0]); // Default label icon
+		add(label);
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
