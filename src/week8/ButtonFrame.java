1c7bd6ffcb1c10347202528a80251a95cba799a6
Wallace Balaniuc <wbalaniu@my.centennialcollege.ca>
2017-03-09 08:27:00 -0500

    Added ButtonFrame and ButtonTest

--- src/week8/ButtonFrame.java
+++ src/week8/ButtonFrame.java
@@ -9,6 +9,49 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
-public class ButtonFrame {
+public class ButtonFrame extends JFrame {
+	private final JButton plainJButton;	// Just text
+	private final JButton fancyJButton; // Icons
+	
+	public ButtonFrame() {
+		super("Testing buttons");
+		setLayout(new FlowLayout());
+		
+		plainJButton = new JButton("Plain Button");
+		add(plainJButton);
+		
+		Icon bug1 = new ImageIcon("images/bug1.png");
+		Icon bug2 = new ImageIcon("images/bug2.gif");
+		fancyJButton = new JButton("Fancy Button", bug1);
+		fancyJButton.setRolloverIcon(bug2);
+		add(fancyJButton);
+		
+		ButtonHandler handler = new ButtonHandler();
+		plainJButton.addActionListener(handler);
+		fancyJButton.addActionListener(handler);
+	}
+	
+	private class ButtonHandler implements ActionListener {
 
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			JOptionPane.showMessageDialog(ButtonFrame.this, 
+					String.format("You pressed: %s", e.getActionCommand()));
+		}
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
