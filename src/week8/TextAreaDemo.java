502f5632b0f77853f9c621d6fedbed9f9ba01034
Wallace Balaniuc <wbalaniu@my.centennialcollege.ca>
2017-03-16 08:29:55 -0400

    Added TextArea demostration

---
+++ src/week8/TextAreaDemo.java
@@ -0,0 +1,14 @@
+package week8;
+
+import javax.swing.JFrame;
+
+public class TextAreaDemo
+{
+   public static void main(String[] args)
+   { 
+      TextAreaFrame textAreaFrame = new TextAreaFrame(); 
+      textAreaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      textAreaFrame.setSize(425, 200); 
+      textAreaFrame.setVisible(true); 
+   } 
+} // end class TextAreaDemo
